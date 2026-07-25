daaa25f0e0882c0f8c0159f0a88994640c812b79
Prashant-00000 <23btrcn053@jainuniversity.ac.in>
2026-07-25 11:21:12 +0530

    Remove temporary enqueue script

--- test_enqueue.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-subprocess.run(['python', '-m', 'queuectl.cli', 'enqueue', '{"command": "echo Hello"}'])
-subprocess.run(['python', '-m', 'queuectl.cli', 'enqueue', '{"command": "python --version"}'])
